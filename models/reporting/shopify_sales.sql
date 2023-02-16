8f28fbbe630cf6d9a852165192b327a5631c89ad
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-02-16 17:03:13 +0100

    Update shopify_sales.sql

--- models/reporting/shopify_sales.sql
+++ models/reporting/shopify_sales.sql
@@ -39,9 +39,9 @@ WITH
         COALESCE(SUM(CASE WHEN customer_order_index > 1 THEN subtotal_discount+shipping_discount END),0) as repeat_order_discounts,
         COALESCE(SUM(CASE WHEN customer_order_index > 1 THEN subtotal_discount END),0) as repeat_order_subtotal_discounts,
         COALESCE(SUM(CASE WHEN customer_order_index > 1 THEN shipping_discount END),0) as repeat_order_shipping_discounts,
-        SUM(COALESCE(gross_revenue,0) - COALESCE(subtotal_discount+shipping_discount,0)) as subtotal_sales,
-        COALESCE(SUM(CASE WHEN customer_order_index = 1 THEN subtotal_revenue END),0) as first_order_subtotal_sales,
-        COALESCE(SUM(CASE WHEN customer_order_index > 1 THEN subtotal_revenue END),0) as repeat_order_subtotal_sales,
+        SUM(COALESCE(gross_revenue,0) - COALESCE(subtotal_discount,0)) as subtotal_sales,
+        COALESCE(SUM(CASE WHEN customer_order_index = 1 THEN COALESCE(gross_revenue,0) - COALESCE(subtotal_discount,0) END),0) as first_order_subtotal_sales,
+        COALESCE(SUM(CASE WHEN customer_order_index > 1 THEN COALESCE(gross_revenue,0) - COALESCE(subtotal_discount,0) END),0) as repeat_order_subtotal_sales,
         COALESCE(SUM(total_tax),0) as gross_tax, 
         COALESCE(SUM(shipping_price),0) as gross_shipping,
         COALESCE(SUM(subtotal_revenue+COALESCE(total_tax,0)+COALESCE(shipping_price,0)),0) as total_sales,
@@ -59,8 +59,8 @@ SELECT
     coalesce(r.subtotal_refund,0) as subtotal_returns,
     coalesce(r.shipping_refund,0) as shipping_returns,
     coalesce(r.tax_refund,0) as tax_returns,
-    s.subtotal_sales - coalesce(r.subtotal_refund,0) as subtotal_net_sales,
-    s.total_sales - coalesce(r.total_refund,0) as net_sales
+    s.subtotal_sales - coalesce(r.subtotal_refund,0) as net_sales,
+    s.total_sales - coalesce(r.total_refund,0) as total_net_sales
 FROM sales_{{date_granularity}} s
 LEFT JOIN refunds_{{date_granularity}} r USING(date_granularity, date)
 {% if not loop.last %}UNION ALL
