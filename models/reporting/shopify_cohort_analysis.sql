c383b6857cce358b0ad93e2c48fc3d78164549d6
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-03-10 21:51:18 +0100

    Update shopify_cohort_analysis.sql

--- models/reporting/shopify_cohort_analysis.sql
+++ models/reporting/shopify_cohort_analysis.sql
@@ -14,7 +14,7 @@ WITH
     
     cohort_size_{{date_granularity}} AS(
       SELECT 
-        DATE_TRUNC({{date_granularity}}, customer_acquisition_day::date) AS cohort,
+        DATE_TRUNC('{{date_granularity}}', customer_acquisition_day::date) AS cohort,
         COUNT(DISTINCT customer_id) AS new_customers
       FROM orders 
       GROUP BY cohort 
@@ -23,8 +23,8 @@ WITH
 
     cohort_arpu_and_repeat_rate_{{date_granularity}} AS(
       SELECT 
-        DATE_TRUNC( {{date_granularity}}, customer_acquisition_day) AS cohort,
-        FLOOR(DATEDIFF(day, customer_acquisition_day, order_date)/CASE {{date_granularity}} WHEN 'week' THEN 7 WHEN 'month' THEN 30.42 WHEN 'quarter' THEN 91.25 END)+1 AS retention,
+        DATE_TRUNC( '{{date_granularity}}', customer_acquisition_day) AS cohort,
+        FLOOR(DATEDIFF(day, customer_acquisition_day, order_date)/CASE '{{date_granularity}}' WHEN 'week' THEN 7 WHEN 'month' THEN 30.42 WHEN 'quarter' THEN 91.25 END)+1 AS retention,
         COALESCE(SUM(SUM(subtotal_revenue)) over (partition BY cohort ORDER BY retention rows between unbounded preceding AND current row)::decimal
         /NULLIF(SUM(COUNT(CASE WHEN customer_order_index = 1 THEN customer_id END)) over (partition BY cohort),0),0) AS arpu,
         COALESCE(SUM(COUNT(CASE WHEN customer_order_index = 2 THEN customer_id END)) over (partition by cohort ORDER BY retention rows unbounded preceding)::float
