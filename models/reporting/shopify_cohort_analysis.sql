2f798ce4a00a0914e4590a2a54d70e2a10687fa2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-03-10 17:06:30 +0100

    Update shopify_cohort_analysis.sql

--- models/reporting/shopify_cohort_analysis.sql
+++ models/reporting/shopify_cohort_analysis.sql
@@ -7,7 +7,7 @@
 WITH 
     orders AS (
       SELECT order_id, date AS order_date, subtotal_revenue, customer_id, customer_acquisition_date::date AS customer_acquisition_day, customer_order_index
-      FROM reporting.wolfproject_shopify_daily_sales_by_order
+      FROM {{ ref('shopify_daily_sales_by_order') }}
     ),
     
     {%- for date_granularity in date_granularity_list %}
