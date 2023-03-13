e190b9d0a20d1f38727c64a6c16cf928fdb4ed21
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-03-13 12:28:23 +0100

    Update shopify_cohort_analysis.sql

--- models/reporting/shopify_cohort_analysis.sql
+++ models/reporting/shopify_cohort_analysis.sql
@@ -15,15 +15,17 @@ WITH
     cohort_size_{{date_granularity}} AS(
       SELECT 
         DATE_TRUNC('{{date_granularity}}', customer_acquisition_day::date) AS cohort,
+        '{{date_granularity}}' as date_granularity,
         COUNT(DISTINCT customer_id) AS new_customers
       FROM orders 
-      GROUP BY cohort 
-      ORDER BY cohort
+      GROUP BY cohort, date_granularity 
+      ORDER BY cohort, date_granularity
     ),
 
     cohort_arpu_and_repeat_rate_{{date_granularity}} AS(
       SELECT 
         DATE_TRUNC( '{{date_granularity}}', customer_acquisition_day) AS cohort,
+        '{{date_granularity}}' as date_granularity,
         FLOOR(DATEDIFF(day, customer_acquisition_day, order_date)/CASE '{{date_granularity}}' WHEN 'week' THEN 7 WHEN 'month' THEN 30.42 WHEN 'quarter' THEN 91.25 END)+1 AS retention,
         COALESCE(SUM(SUM(subtotal_revenue)) over (partition BY cohort ORDER BY retention rows between unbounded preceding AND current row)::decimal
         /NULLIF(SUM(COUNT(CASE WHEN customer_order_index = 1 THEN customer_id END)) over (partition BY cohort),0),0) AS arpu,
@@ -32,8 +34,8 @@ WITH
     FROM orders 
     WHERE 1=1
     AND cohort IS NOT NULL
-    GROUP BY cohort, retention 
-    ORDER BY cohort, retention
+    GROUP BY cohort, date_granularity, retention 
+    ORDER BY cohort, date_granularity, retention
     ){%- if not loop.last %},{%- endif %}
     {%- endfor %}
 
