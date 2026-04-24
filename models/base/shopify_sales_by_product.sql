650141f983e0a7807346be3f05fff4703337547f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-24 15:36:45 -0400

    Add SQL query for Shopify sales by product

--- models/base/shopify_sales_by_product.sql
+++ models/base/shopify_sales_by_product.sql
@@ -1,1 +1,45 @@
+{{ config(
+    alias = target.database + '_shopify_sales_by_product'
+) }}
 
+
+WITH shopify_data AS (
+
+    -- SALES ROWS
+    SELECT
+
+        order_date,
+        order_line_id,
+        product_id,
+        title,
+        name,
+        sku,
+  
+        -- Gross Revenue
+        SUM(COALESCE(price * quantity,0)) AS order_gross_revenue,
+        SUM(COALESCE(price * refund_quantity,0)) AS refund_gross_revenue,
+
+        -- Quantity
+        SUM(COALESCE(quantity,0)) AS order_quantity,
+        SUM(COALESCE(refund_quantity,0)) AS refund_quantity,
+  
+
+        -- Subtotal Discounts
+        SUM(COALESCE(discount_amount,0)) AS subtotal_discount,
+
+        -- Subtotal Refunds
+        SUM(COALESCE(refund_subtotal,0)) AS subtotal_refund,
+
+        -- Net Quantity
+        SUM(COALESCE(quantity) - COALESCE(refund_quantity)) as net_quantity,
+
+        -- Net Subtotal
+        SUM(COALESCE(price * quantity) - COALESCE(refund_subtotal)) as net_subtotal
+
+
+    FROM {{ ref('shopify_line_items') }}
+  group by 1,2,3,4,5,6
+
+
+SELECT *
+FROM shopify_data
