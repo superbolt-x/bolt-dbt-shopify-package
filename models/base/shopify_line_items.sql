cb1976cd4497994f7c0251a7cd9f2910f367d39d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-24 14:45:03 -0400

    Add discount allocation fields to Shopify line items

--- models/base/shopify_line_items.sql
+++ models/base/shopify_line_items.sql
@@ -1,6 +1,7 @@
 {%- set schema_name,
         item_table_name, 
-        = 'shopify_raw', 'order_line' -%}
+        item_discount_table_name
+        = 'shopify_raw', 'order_line', 'discount_allocation' -%}
 {%- set item_selected_fields = [
     "order_id",
     "id",
@@ -18,7 +19,14 @@
     "index"
 ] -%}
 
+{%- set item_discount_selected_fields = [
+"order_line_id",
+"amount",
+] -%}
+
 {%- set order_line_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line') -%}
+{%- set order_line_discount_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'discount_allocation') -%}
+
 WITH order_line_raw_data AS 
     ({{ dbt_utils.union_relations(relations = order_line_raw_tables) }}),
     items AS 
@@ -30,5 +38,20 @@ WITH order_line_raw_data AS
     FROM order_line_raw_data
     )
 
+order_line_discount_raw_data AS 
+        ({{ dbt_utils.union_relations(relations = order_line_discount_raw_tables) }}),
+
+    discount AS 
+    (SELECT 
+        
+        {% for column in item_discount_selected_fields -%}
+        {{ get_shopify_clean_field(item_discount_table_name, column)}}
+        {%- if not loop.last %},{% endif %}
+        {% endfor %}
+    FROM order_line_discount_raw_data
+    ),
+
 SELECT *
 FROM items 
+left join discount using (order_line_id)
+
