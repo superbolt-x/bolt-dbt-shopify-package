4ef9795147a85c66fe3b3dfdda49b01060e8176e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-24 14:04:16 -0400

    Simplify Shopify line items SQL by removing refunds
    Removed refund-related fields and logic from the SQL query.

--- models/base/shopify_line_items.sql
+++ models/base/shopify_line_items.sql
@@ -1,7 +1,6 @@
 {%- set schema_name,
         item_table_name, 
-        item_fund_table_name
-        = 'shopify_raw', 'order_line', 'order_line_refund' -%}
+        = 'shopify_raw', 'order_line' -%}
 {%- set item_selected_fields = [
     "order_id",
     "id",
@@ -18,14 +17,8 @@
     "gift_card",
     "index"
 ] -%}
-{%- set item_refund_selected_fields = [
-    "order_line_id",
-    "refund_id",
-    "quantity",
-    "subtotal"
-] -%}
+
 {%- set order_line_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line') -%}
-{%- set order_line_refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line_refund') -%}
 WITH order_line_raw_data AS 
     ({{ dbt_utils.union_relations(relations = order_line_raw_tables) }}),
     items AS 
@@ -35,30 +28,7 @@ WITH order_line_raw_data AS
         {%- if not loop.last %},{% endif %}
         {% endfor %}
     FROM order_line_raw_data
-    ),
-    order_line_refund_raw_data AS 
-    ({{ dbt_utils.union_relations(relations = order_line_refund_raw_tables) }}),
-    refund_raw AS 
-    (SELECT 
-        
-        {% for column in item_refund_selected_fields -%}
-        {{ get_shopify_clean_field(item_fund_table_name, column)}}
-        {%- if not loop.last %},{% endif %}
-        {% endfor %}
-    FROM order_line_refund_raw_data
-    ),
-    refund AS 
-    (SELECT 
-        order_line_id,
-        SUM(refund_quantity) as refund_quantity,
-        SUM(refund_subtotal) as refund_subtotal
-    FROM refund_raw
-    GROUP BY order_line_id
     )
 
-SELECT *,
-    quantity - refund_quantity as net_quantity,
-    price * quantity - refund_subtotal as net_subtotal,
-    order_line_id as unique_key
+SELECT *
 FROM items 
-LEFT JOIN refund USING(order_line_id)
