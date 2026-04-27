cb454002461ff7e02a569c48eeb437a69a5f24b6
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-27 16:26:51 +0200

    Update shopify_daily_sales_by_order.sql

--- models/reporting/shopify_daily_sales_by_order.sql
+++ models/reporting/shopify_daily_sales_by_order.sql
@@ -1,71 +1,28 @@
-{{ config (
+{{ config(
     alias = target.database + '_shopify_daily_sales_by_order',
     materialized='incremental',
     unique_key='unique_key'
-)}}
+) }}
 
 {# -------------------- VARS -------------------- #}
-{%- set sales_channel_exclusion = var("sales_channel_exclusion", "") -%}
-{%- set sales_channel_inclusion = var("sales_channel_inclusion", "") -%}
-{%- set shipping_countries_excluded = var("shipping_countries_excluded", "") -%}
-{%- set shipping_countries_included = var("shipping_countries_included", "") -%}
-{%- set order_tags_keyword_exclusion = var("order_tags_keyword_exclusion", "") -%}
-{%- set order_tags_keyword_inclusion = var("order_tags_keyword_inclusion", "") -%}
-{%- set email_address_exclusion = var("email_address_exclusion", "") -%}
+{%- set sales_channel_exclusion = var("sales_channel_exclusion", "") | trim -%}
+{%- set sales_channel_inclusion = var("sales_channel_inclusion", "") | trim -%}
 
-{# -------------------- LISTS -------------------- #}
-{%- set sales_channel_exclusion_values =
-    sales_channel_exclusion.split('|')
-    | map('trim')
-    | reject('equalto','')
-    | list
--%}
+{%- set shipping_countries_excluded = var("shipping_countries_excluded", "") | trim -%}
+{%- set shipping_countries_included = var("shipping_countries_included", "") | trim -%}
 
-{%- set sales_channel_inclusion_values =
-    sales_channel_inclusion.split('|')
-    | map('trim')
-    | reject('equalto','')
-    | list
--%}
+{%- set order_tags_keyword_exclusion = var("order_tags_keyword_exclusion", "") | trim -%}
+{%- set order_tags_keyword_inclusion = var("order_tags_keyword_inclusion", "") | trim -%}
 
-{%- set shipping_country_exclusion_values =
-    shipping_countries_excluded.split('|')
-    | map('trim')
-    | reject('equalto','')
-    | list
--%}
-
-{%- set shipping_country_inclusion_values =
-    shipping_countries_included.split('|')
-    | map('trim')
-    | reject('equalto','')
-    | list
--%}
-
-{%- set sales_channel_exclusion_list =
-    "'" ~ sales_channel_exclusion_values | join("','") ~ "'"
-    if sales_channel_exclusion_values | length > 0 else none
--%}
-
-{%- set sales_channel_inclusion_list =
-    "'" ~ sales_channel_inclusion_values | join("','") ~ "'"
-    if sales_channel_inclusion_values | length > 0 else none
--%}
-
-{%- set shipping_country_exclusion_list =
-    "'" ~ shipping_country_exclusion_values | join("','") ~ "'"
-    if shipping_country_exclusion_values | length > 0 else none
--%}
-
-{%- set shipping_country_inclusion_list =
-    "'" ~ shipping_country_inclusion_values | join("','") ~ "'"
-    if shipping_country_inclusion_values | length > 0 else none
--%}
+{%- set email_address_exclusion = var("email_address_exclusion", "") | trim -%}
 
 WITH giftcard_deduction AS (
     SELECT 
-        order_id, 
-        CASE WHEN items_count = giftcard_count THEN 'true' ELSE 'false' END AS giftcard_only,
+        order_id,
+        CASE 
+            WHEN items_count = giftcard_count THEN 'true' 
+            ELSE 'false' 
+        END AS giftcard_only,
         giftcard_deduction
     FROM (
         SELECT 
@@ -86,48 +43,61 @@ orders AS (
         order_id,
         customer_id,
         customer_order_index,
+
         gross_revenue - COALESCE(giftcard_deduction,0) AS gross_revenue,
         total_discounts - gross_revenue + subtotal_revenue AS shipping_discount,
         gross_revenue - subtotal_revenue AS subtotal_discount,
+
         discount_rate,
         subtotal_revenue,
         total_tax,
         shipping_price,
         total_revenue,
+
         order_tags,
         email,
         source_name,
         shipping_address_country_code
+
     FROM {{ ref('shopify_orders') }}
     LEFT JOIN giftcard_deduction USING(order_id)
+
     WHERE giftcard_only = 'false'
 
-    {# -------- SALES CHANNEL -------- #}
-    {% if sales_channel_inclusion_list %}
-        AND source_name IN ({{ sales_channel_inclusion_list }})
-    {% elif sales_channel_exclusion_list %}
-        AND (source_name NOT IN ({{ sales_channel_exclusion_list }}) OR source_name IS NULL)
+    {# -------------------- SALES CHANNEL -------------------- #}
+    {% if sales_channel_inclusion %}
+        AND source_name IN (
+            '{{ sales_channel_inclusion | replace("|", "','") }}'
+        )
+    {% elif sales_channel_exclusion %}
+        AND source_name NOT IN (
+            '{{ sales_channel_exclusion | replace("|", "','") }}'
+        )
     {% endif %}
 
-    {# -------- TAGS -------- #}
-    {% if order_tags_keyword_exclusion | trim %}
+    {# -------------------- SHIPPING COUNTRY -------------------- #}
+    {% if shipping_countries_included %}
+        AND shipping_address_country_code IN (
+            '{{ shipping_countries_included | replace("|", "','") }}'
+        )
+    {% elif shipping_countries_excluded %}
+        AND shipping_address_country_code NOT IN (
+            '{{ shipping_countries_excluded | replace("|", "','") }}'
+        )
+    {% endif %}
+
+    {# -------------------- TAGS -------------------- #}
+    {% if order_tags_keyword_exclusion %}
         AND (order_tags !~* '{{ order_tags_keyword_exclusion }}' OR order_tags IS NULL)
     {% endif %}
 
-    {% if order_tags_keyword_inclusion | trim %}
+    {% if order_tags_keyword_inclusion %}
         AND order_tags ~* '{{ order_tags_keyword_inclusion }}'
     {% endif %}
 
-    {# -------- EMAIL -------- #}
-    {% if email_address_exclusion | trim %}
+    {# -------------------- EMAIL -------------------- #}
+    {% if email_address_exclusion %}
         AND (email !~* '{{ email_address_exclusion }}' OR email IS NULL)
-    {% endif %}
-
-    {# -------- SHIPPING COUNTRY -------- #}
-    {% if shipping_country_inclusion_list %}
-        AND shipping_address_country_code IN ({{ shipping_country_inclusion_list }})
-    {% elif shipping_country_exclusion_list %}
-        AND (shipping_address_country_code NOT IN ({{ shipping_country_exclusion_list }}) OR shipping_address_country_code IS NULL)
     {% endif %}
 )
 
