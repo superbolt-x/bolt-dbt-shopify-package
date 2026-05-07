8a320d007701e264f8b2650518a6b9070d6a4cf1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-05-07 12:44:23 -0400

    Simplify Shopify refunds line items SQL query
    Refactor SQL query to focus on refund data, removing unused item and discount selections.

---
+++ models/base/shopify_refunds_line_items.sql
@@ -0,0 +1,46 @@
+{%- set schema_name,item_refund_table_name = 'shopify_raw', 'order_line_refund' -%}
+  
+
+
+{%- set item_refund_selected_fields = [
+  "id",
+"order_line_id",
+"refund_id",
+"quantity",
+"subtotal"
+] -%}
+
+
+{%- set order_line_refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line_refund') -%}
+
+with
+order_line_refund_raw_data AS 
+    ({{ dbt_utils.union_relations(relations = order_line_refund_raw_tables) }}),
+        
+    refund_raw AS 
+    (SELECT 
+        
+        {% for column in item_refund_selected_fields -%}
+        {{ get_shopify_clean_field(item_refund_table_name, column)}}
+        {%- if not loop.last %},{% endif %}
+        {% endfor %}
+    FROM order_line_refund_raw_data
+    ),
+        refund AS 
+    (SELECT 
+        order_line_id,
+        date,
+        SUM(quantity) as refund_quantity,
+        SUM(subtotal) as refund_subtotal
+    FROM refund_raw r
+        left join {{ ref('shopify_refunds') }} s
+        on r.refund_id = s.refund_id
+    GROUP BY order_line_id, date
+    )
+
+SELECT 
+        order_line_id,
+        date,
+        refund_quantity,
+        refund_subtotal
+FROM refund 
