d81153a9a25b0f2c5b43a48e8b0a55c94581db6f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-05-07 22:32:47 -0400

    Update shopify_refund_line_items.sql

--- models/base/shopify_refund_line_items.sql
+++ models/base/shopify_refund_line_items.sql
@@ -1,5 +1,8 @@
-{%- set schema_name, item_refund_table_name, item_refund_table_name = 'shopify_raw', 'order_line', 'order_line_refund' -%}
-  
+{%- set schema_name,
+        item_table_name,
+        item_refund_table_name
+        = 'shopify_raw', 'order_line', 'order_line_refund' -%}
+
 {%- set item_selected_fields = [
     "order_id",
     "id",
@@ -18,81 +21,115 @@
 ] -%}
 
 {%- set item_refund_selected_fields = [
-  "id",
-"order_line_id",
-"refund_id",
-"quantity",
-"subtotal"
+    "id",
+    "order_line_id",
+    "refund_id",
+    "quantity",
+    "subtotal"
 ] -%}
 
 {%- set order_line_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line') -%}
 {%- set order_line_refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line_refund') -%}
 
-  
-  WITH order_line_raw_data AS 
-    ({{ dbt_utils.union_relations(relations = order_line_raw_tables) }}),
-        
-    orders AS 
-    (SELECT 
+WITH order_line_raw_data AS (
+
+    {{ dbt_utils.union_relations(relations = order_line_raw_tables) }}
+
+),
+
+orders AS (
+
+    SELECT 
+        r.id AS order_line_id,
+
         {% for column in item_selected_fields -%}
-        {{ get_shopify_clean_field(item_table_name, column)}}
+        {{ get_shopify_clean_field(item_table_name, column) }}
         {%- if not loop.last %},{% endif %}
         {% endfor %}
+
     FROM order_line_raw_data r
-        left join {{ ref('shopify_orders') }} s
-        on r.order_id = s.order_id
-    ),
-  
-order_line_refund_raw_data AS 
-    ({{ dbt_utils.union_relations(relations = order_line_refund_raw_tables) }}),
-        
-    refund_raw AS 
-    (SELECT 
+    LEFT JOIN {{ ref('shopify_orders') }} s
+        ON r.order_id = s.order_id
+
+),
+
+order_line_refund_raw_data AS (
+
+    {{ dbt_utils.union_relations(relations = order_line_refund_raw_tables) }}
+
+),
+
+refund_raw AS (
+
+    SELECT 
         
         {% for column in item_refund_selected_fields -%}
-        {{ get_shopify_clean_field(item_refund_table_name, column)}}
+        {{ get_shopify_clean_field(item_refund_table_name, column) }}
         {%- if not loop.last %},{% endif %}
         {% endfor %}
+
     FROM order_line_refund_raw_data
-    ),
-        refund AS 
-    (SELECT 
-        order_line_id,
-        refund_date,
-        day,
-        week,
-        month,
-        quarter,
-        year,
-        SUM(quantity) as refund_quantity,
-        SUM(subtotal) as refund_subtotal
+
+),
+
+refund AS (
+
+    SELECT 
+        r.order_line_id,
+
+        s.refund_date,
+        s.day,
+        s.week,
+        s.month,
+        s.quarter,
+        s.year,
+
+        SUM(r.quantity) AS refund_quantity,
+        SUM(r.subtotal) AS refund_subtotal
+
     FROM refund_raw r
-        left join {{ ref('shopify_refunds') }} s
-        on r.refund_id = s.refund_id
-    GROUP BY order_line_id, refund_date
-    )
+
+    LEFT JOIN {{ ref('shopify_refunds') }} s
+        ON r.refund_id = s.refund_id
+
+    GROUP BY
+        r.order_line_id,
+        s.refund_date,
+        s.day,
+        s.week,
+        s.month,
+        s.quarter,
+        s.year
+
+)
+
 SELECT 
-        refund_date,
-        day,
-        week,
-        month,
-        quarter,
-        year,
-        refund_quantity,
-        refund_subtotal,
-        order_line_id,
-        id,
-        product_id,
-        variant_id,
-        title,
-        variant_title,
-        name,
-        price,
-        sku,
-        fulfillable_quantity,
-        fulfillment_status,
-        gift_card,
-        index,        
-FROM orders 
-  left join refund using (order_line_id)
+    r.refund_date,
+    r.day,
+    r.week,
+    r.month,
+    r.quarter,
+    r.year,
+
+    r.refund_quantity,
+    r.refund_subtotal,
+
+    o.order_line_id,
+    o.order_id,
+    o.id,
+    o.product_id,
+    o.variant_id,
+    o.title,
+    o.variant_title,
+    o.name,
+    o.price,
+    o.sku,
+    o.fulfillable_quantity,
+    o.fulfillment_status,
+    o.gift_card,
+    o.index
+
+FROM orders o
+LEFT JOIN refund r
+    USING (order_line_id)
 
