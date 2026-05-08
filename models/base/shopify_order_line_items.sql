466a2f0fbb780e2280dab0ca86c00ef02a4f2d70
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-05-07 22:32:27 -0400

    Refactor SQL for Shopify order line items

--- models/base/shopify_order_line_items.sql
+++ models/base/shopify_order_line_items.sql
@@ -1,10 +1,8 @@
 {%- set schema_name,
         item_table_name, 
-        item_discount_table_name,
-        item_fund_table_name
-        = 'shopify_raw', 'order_line', 'discount_allocation', 'order_line_refund' -%}
+        item_discount_table_name
+        = 'shopify_raw', 'order_line', 'discount_allocation' -%}
 
-        
 {%- set item_selected_fields = [
     "order_id",
     "id",
@@ -23,73 +21,96 @@
 ] -%}
 
 {%- set item_discount_selected_fields = [
-"order_line_id",
-"amount"
+    "order_line_id",
+    "amount"
 ] -%}
-
 
 {%- set order_line_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line') -%}
 {%- set discount_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'discount_allocation') -%}
-{%- set order_line_refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line_refund') -%}
 
+WITH order_line_raw_data AS (
 
-WITH order_line_raw_data AS 
-    ({{ dbt_utils.union_relations(relations = order_line_raw_tables) }}),
-        
-    orders AS 
-    (SELECT 
-        order_date,
-        day,
-        week,
-        month,
-        quarter,
-        year,
+    {{ dbt_utils.union_relations(relations = order_line_raw_tables) }}
+
+),
+
+orders AS (
+
+    SELECT 
+        s.order_date,
+        s.day,
+        s.week,
+        s.month,
+        s.quarter,
+        s.year,
+
+        r.id AS order_line_id,
+
         {% for column in item_selected_fields -%}
-        {{ get_shopify_clean_field(item_table_name, column)}}
+        {{ get_shopify_clean_field(item_table_name, column) }}
         {%- if not loop.last %},{% endif %}
         {% endfor %}
+
     FROM order_line_raw_data r
-        left join {{ ref('shopify_orders') }} s
-        on r.order_id = s.order_id
-    ),
+    LEFT JOIN {{ ref('shopify_orders') }} s
+        ON r.order_id = s.order_id
 
-discount_raw_data AS 
-        ({{ dbt_utils.union_relations(relations = discount_raw_tables) }}),
+),
 
-    discount AS 
-    (SELECT 
+discount_raw_data AS (
+
+    {{ dbt_utils.union_relations(relations = discount_raw_tables) }}
+
+),
+
+discount AS (
+
+    SELECT 
+        order_line_id,
+        SUM(amount) AS discount_amount
+
+    FROM (
+
+        SELECT
         
-        {% for column in item_discount_selected_fields -%}
-        {{ get_shopify_clean_field(item_discount_table_name, column)}}
-        {%- if not loop.last %},{% endif %}
-        {% endfor %}
-    FROM discount_raw_data
-    ),
+            {% for column in item_discount_selected_fields -%}
+            {{ get_shopify_clean_field(item_discount_table_name, column) }}
+            {%- if not loop.last %},{% endif %}
+            {% endfor %}
 
+        FROM discount_raw_data
 
+    )
+
+    GROUP BY 1
+
+)
 
 SELECT 
-        order_line_id,
-        id,
-        order_date,
-        day,
-        week,
-        month,
-        quarter,
-        year,
-        product_id,
-        variant_id,
-        title,
-        variant_title,
-        name,
-        price,
-        quantity,
-        sku,
-        fulfillable_quantity,
-        fulfillment_status,
-        gift_card,
-        index,
-        amount as discount_amount
-        
-FROM orders 
-left join discount using (order_line_id)
+    o.order_line_id,
+    o.order_id,
+    o.id,
+    o.order_date,
+    o.day,
+    o.week,
+    o.month,
+    o.quarter,
+    o.year,
+    o.product_id,
+    o.variant_id,
+    o.title,
+    o.variant_title,
+    o.name,
+    o.price,
+    o.quantity,
+    o.sku,
+    o.fulfillable_quantity,
+    o.fulfillment_status,
+    o.gift_card,
+    o.index,
+
+    COALESCE(d.discount_amount, 0) AS discount_amount
+
+FROM orders o
+LEFT JOIN discount d
+    USING (order_line_id)
