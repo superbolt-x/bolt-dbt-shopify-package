0c95389a4a234e65616187544bb38c9b6547ddd5
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-07-15 14:54:29 -0300

    Add subtotal_price and total_price to daily sales report
    Added subtotal_price and total_price calculations to the sales report.

--- models/reporting/shopify_daily_sales_by_order_line_item.sql
+++ models/reporting/shopify_daily_sales_by_order_line_item.sql
@@ -49,7 +49,9 @@ WITH orders AS
         discount_rate,
         (price * quantity) * COALESCE(subtotal_revenue / NULLIF(gross_revenue,0)) as subtotal_sales,
         (price * quantity) * COALESCE(total_revenue / NULLIF(gross_revenue,0)) as total_sales,
-        quantity - COALESCE(refund_quantity,0) as net_quantity
+        quantity - COALESCE(refund_quantity,0) as net_quantity,
+        subtotal_price,
+        total_price
     FROM orders 
     LEFT JOIN line_items USING(order_id)
     )
