29885ba13600e575f6ee054c61a384afcadc24e7
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-08-27 11:19:32 -0300

    Refactor line refund data handling in SQL
    Removed line_refund_raw_tables variable and updated line_refund_raw_data to use get_shopify_refund_line_items function.

--- models/base/shopify_orders.sql
+++ models/base/shopify_orders.sql
@@ -112,7 +112,6 @@
 {%- set tag_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_tag') -%}
 {%- set refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'refund') -%}
 {%- set adjustment_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_adjustment') -%}
-{%- set line_refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'order_line_refund') -%}
 {%- set fulfillment_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw%', 'fulfillment') -%}
 {%- set has_fulfillment = fulfillment_raw_tables | length > 0 -%}
 
@@ -232,7 +231,7 @@ WITH
     ),
 
     line_refund_raw_data AS
-    ({{ dbt_utils.union_relations(relations = line_refund_raw_tables) }}),
+    ({{ get_shopify_refund_line_items() }}),
 
     line_refund_staging AS
     (SELECT
